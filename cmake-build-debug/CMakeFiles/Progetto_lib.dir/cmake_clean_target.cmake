b62da11bf0d6d39a0f6cf8eda162289d900798ce
niccolomagnolfi <niccomagno@icloud.com>
2024-07-16 19:19:14 +0200

    Aggiunta cartella googletest

---
+++ cmake-build-debug/CMakeFiles/Progetto_lib.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "libProgetto_lib.a"
+)
---
+++ cmake-build-debug/test/CMakeFiles/runAllTests.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/runAllTests.dir/CollezioneFixture.cpp.o"
+  "CMakeFiles/runAllTests.dir/CollezioneFixture.cpp.o.d"
+  "CMakeFiles/runAllTests.dir/CollezioneTest.cpp.o"
+  "CMakeFiles/runAllTests.dir/CollezioneTest.cpp.o.d"
+  "CMakeFiles/runAllTests.dir/NotaFIxture.cpp.o"
+  "CMakeFiles/runAllTests.dir/NotaFIxture.cpp.o.d"
+  "CMakeFiles/runAllTests.dir/NotaTest.cpp.o"
+  "CMakeFiles/runAllTests.dir/NotaTest.cpp.o.d"
+  "CMakeFiles/runAllTests.dir/runAllTests.cpp.o"
+  "CMakeFiles/runAllTests.dir/runAllTests.cpp.o.d"
+  "runAllTests"
+  "runAllTests.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/runAllTests.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
